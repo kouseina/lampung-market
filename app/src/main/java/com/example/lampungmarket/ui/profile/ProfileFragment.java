6d4caf6c549563187743e318a4899c56a997a4b1
kouseina <kouseinadaffa@gmail.com>
2023-07-07 18:49:39 +0700

    feat(main): add home, search, profile

--- app/src/main/java/com/example/lampungmarket/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/lampungmarket/ui/profile/ProfileFragment.java
@@ -1,4 +1,4 @@
-package com.example.lampungmarket.ui.dashboard;
+package com.example.lampungmarket.ui.profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,20 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.lampungmarket.databinding.FragmentDashboardBinding;
+import com.example.lampungmarket.databinding.FragmentProfileBinding;
 
-public class DashboardFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentProfileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        ProfileViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = com.example.lampungmarket.databinding.FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
